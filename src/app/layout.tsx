eb4c990019d0f87d395cda37e04bbab6618c09ad
Rabia Inam <devwithrabia@gmail.com>
2025-02-17 22:01:53 +0500

    change meta data title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Rabia Restaurant',
+  title: 'MASSIMO',
   description: 'Best food in town!'
 }
 
