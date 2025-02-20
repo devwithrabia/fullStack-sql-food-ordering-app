26a6894be472c6761d0e45bd2070eeb618c24d3c
Rabia Inam <devwithrabia@gmail.com>
2025-02-20 10:38:34 +0500

    change image

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -16,7 +16,7 @@ const data: any = [
   {
     id: 3,
     title: 'the best burger to share with your family',
-    image: '/cheese.jpg'
+    image: '/pastac.jpg'
   }
 ]
 const Slider = () => {
@@ -31,16 +31,14 @@ const Slider = () => {
   }, [])
 
   return (
-    <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
-      <div className='flex-1 h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold lg:h-full'>
+    <div className='flex flex-wrap h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]  bg-fuchsia-50'>
+      <div className='flex-1 min-w-[500px] h-auto min-h-[50%] flex items-center justify-center flex-col gap-8 text-red-500 font-bold '>
         <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl md:p-10 '>
           {data[currentSlide].title}
         </h1>
-
-        <button className='bg-red-500 text-white py-4 px-8 '>Order Now</button>
       </div>
 
-      <div className=' flex-1 w-full h-1/2 relative lg:h-full'>
+      <div className=' flex-1 min-w-[500px] h-auto min-h-[50%] relative '>
         <Image src={data[currentSlide].image} alt='' fill className='object-cover' />
       </div>
     </div>
